57637426c186a497e2b7891098cc69adb2865249
snsar <lethoquyen2002@gmail.com>
2025-05-01 15:09:00 +0700

    Add initial configuration files for Neovim and WezTerm setup

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "GruvboxDark"
+
+-- Đặt hình nền (nếu muốn)
+config.window_background_image = "/home/snsar/Downloads/flower.png"
+config.window_background_image_hsb = { brightness = 0.05, hue = 1.0, saturation = 1.0 }
+
+-- Điều chỉnh opacity cho cửa sổ
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.5
+
+return config
